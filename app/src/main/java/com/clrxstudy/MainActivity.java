3d4113b57e0ddeefafe8c7c967695a1185e8771f
jackChen <751934754@qq.com>
2016-08-10 14:30:35 +0800

    onErrorReturn:拦截异常并返回结果

--- app/src/main/java/com/clrxstudy/MainActivity.java
+++ app/src/main/java/com/clrxstudy/MainActivity.java
@@ -4,12 +4,14 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.clrxstudy.databinding.ActivityMainBinding;
 
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
@@ -27,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 查看onErrorReturn的效果:
+     * onError可以拦截异常，然后自定义返回结果
+     * */
     private void done() {
         Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
@@ -34,10 +40,16 @@ public class MainActivity extends AppCompatActivity {
                 int i = 6/0;
                 subscriber.onNext(i);
             }
+        }).onErrorReturn(new Func1<Throwable, Integer>() {
+            @Override
+            public Integer call(Throwable throwable) {
+                Toast.makeText(MainActivity.this, "I catch this error, and I result the result! without me , it will throw exception", Toast.LENGTH_SHORT).show();
+                return null;
+            }
         }).subscribe(new Action1<Integer>() {
             @Override
             public void call(Integer integer) {
-
+                Toast.makeText(MainActivity.this, "" + integer, Toast.LENGTH_SHORT).show();
             }
         });
     }
